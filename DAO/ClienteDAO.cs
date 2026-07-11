7858c9092e406b7cc61f220fa16052e787ad1b1d
Noa <noaloprg@gmail.com>
2026-07-11 11:05:51 +0200

    feat: crea clases DAO para almacenar en BD

---
+++ DAO/ClienteDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda.DAO
+{
+    // Clase donde se definen las QUERYs SQLITE de la tabla de Clientes
+    internal class ClienteDAO
+    {
+
+        private const String NOMBRE_TABLA = "clientes";
+
+        public static String CreateTable()
+        {
+            return $"CREATE TABLE IF NOT EXISTS {NOMBRE_TABLA} (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "correo VARCHAR," +
+                "apellidos VARCHAR, " +
+                "nombre VARCHAR," +
+                "ciudad VARCHAR," +
+                "comentario TEXT," +
+                "vip INTEGER," +
+                ");";
+        }
+
+        public static String Insert()
+        {
+            return $"INSERT INTO {NOMBRE_TABLA} (correo, apellidos, nombre, ciudad, comentario, vip)" +
+                $" VALUES (@correo, @apellidos, @nombre, @ciudad, @comentario, @vip);" ;
+        }
+
+        public static String DeleteClieneByCorreo()
+        {
+            return $"DELETE FROM {NOMBRE_TABLA} WHERE correo = @correo";
+        }
+
+        public static String UpdateAllFields()
+        {
+            return $"UPDATE {NOMBRE_TABLA} " +
+                $"SET " +
+                $"correo = @correo, " +
+                $"apellidos = @apellidos, " +
+                $"nombre = @nombre, " +
+                $"ciudad = @ciudad, " +
+                $"comentrio =  @comentario, " +
+                $"vip = @vip " +
+                $"WHERE id = @id;";
+        }
+    }
+}
---
+++ DAO/DAOService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda.DAO
+{
+    // Clase que hace de servicio para realizar operaciones sobre la base de datos
+    internal class DAOService
+    {
+        private const String NOMBRE_BD = "sprayAndSole.db";
+        private static String rutaBD = $"Data source= {NOMBRE_BD}";
+        private static String rutaConexion = Path.Combine(FileSystem.AppDataDirectory, rutaBD);
+
+        // metodo privado que crea una conexion con la BD
+        private static SqliteConnection GetConexion()
+        {
+            return new SqliteConnection(rutaConexion);
+        }
+
+        // Crea las tablas iniciales
+        public static async Task<bool> CrearTablas()
+        {
+            try
+            {
+                using (var conexion = GetConexion())
+                {
+                    await conexion.OpenAsync();
+
+                    using (var command = new SqliteCommand(ClienteDAO.CreateTable(), conexion))
+                    {
+                        await command.ExecuteNonQueryAsync();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
